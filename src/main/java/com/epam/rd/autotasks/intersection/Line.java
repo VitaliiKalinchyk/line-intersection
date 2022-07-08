14ee85b589a8f1ea3419d037dbffddda19de8c03
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-08 18:51:08 +0300

    Point intersection

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,13 +1,17 @@
 package com.epam.rd.autotasks.intersection;
 
 public class Line {
-
+    int k,b;
     public Line(int k, int b) {
-
+        this.k=k;
+        this.b=b;
     }
 
     public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+        if (k==other.k)
+            return null;
+        int x=(b-other.b)/(other.k-k);
+        int y=k*x+b;
+        return new Point(x,y);
     }
-
 }
